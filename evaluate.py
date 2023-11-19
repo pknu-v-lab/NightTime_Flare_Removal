91b120b3ad2703dbc93be76b24940edb2b0ca95b
unknown <qut1526@gmail.com>
2023-11-19 18:00:42 +0900

    Update

--- evaluate.py
+++ evaluate.py
@@ -1,33 +1,36 @@
-import os
-from skimage.metrics import mean_squared_error
+import argparse
+from skimage.metrics import peak_signal_noise_ratio as compare_psnr
+from skimage.metrics import structural_similarity as compare_ssim
+from skimage.metrics import mean_squared_error as compare_mse
 from skimage import io
+from torchvision.transforms import ToTensor
 import numpy as np
-from statistics import mean
-import sys
+from glob import glob
+import lpips
+from tqdm import tqdm
+import os
+import logging
+import kornia as K
 
-input_dir = './data/result'
-output_dir = './data/result'
+import warnings
+warnings.filterwarnings("ignore")
 
-input_folder = os.path.join(input_dir, 'input')
-gt_folder = os.path.join(input_dir, 'pred_blend') #pred_scene ? pred_blend?
-mask_folder = os.path.join(input_dir, 'pred_flare')
-
-output_filename = os.path.join(output_dir, 'scores.txt')
-
-mask_type_list = ['glare','streak','global']
-gt_list = sorted(os.listdir(gt_folder))
-
-input_list = [input_image for input_image in os.listdir(input_folder)]
-#input_list = list(map(lambda x: os.path.join(input_folder,x.replace('gt', 'input')), gt_list))
-
-mask_list = list([mask_image for mask_image in os.listdir(mask_folder)])
-#mask_list = list(map(lambda x: os.path.join(mask_folder,x.replace('gt', 'mask')), gt_list))
-
-gt_list = list([gt_image for gt_image in os.listdir(gt_folder)])
-#gt_list = list(map(lambda x: os.path.join(gt_folder,x), gt_list))
-
-img_num = len(gt_list)
-metric_dict = {'glare':[],'streak':[],'global':[]}
+def compare_score(img1,img2,img_seg):
+    # Return the G-PSNR, S-PSNR, Global-PSNR and Score
+    # This module is for the MIPI 2023 Challange: https://codalab.lisn.upsaclay.fr/competitions/9402
+    mask_type_list=['glare','streak','global']
+    metric_dict={'glare':0,'streak':0,'global':0}
+    for mask_type in mask_type_list:
+        mask_area,img_mask=extract_mask(img_seg)[mask_type]
+        if mask_area>0:
+            img_gt_masked=img1*img_mask
+            img_input_masked=img2*img_mask
+            input_mse=compare_mse(img_gt_masked, img_input_masked)/(255*255*mask_area)
+            input_psnr=10 * np.log10((1.0 ** 2) / input_mse)
+            metric_dict[mask_type]=input_psnr
+        else:
+            metric_dict.pop(mask_type)
+    return metric_dict
 
 def extract_mask(img_seg):
     # Return a dict with 3 masks including streak,glare,global(whole image w/o light source), masks are returned in 3ch. 
@@ -44,28 +47,65 @@ def extract_mask(img_seg):
     mask_dict['global']=[np.sum(global_mask)/(512*512),np.expand_dims(global_mask,2).repeat(3,axis=2)] 
     return mask_dict
 
-for i in range(img_num):
-    img_gt=io.imread(os.path.join(gt_folder,gt_list[i]))
-    img_input=io.imread(os.path.join(input_folder,input_list[i]))
-    img_seg=io.imread(os.path.join(mask_folder,mask_list[i]))
-    for mask_type in mask_type_list:
-        mask_area,img_mask=extract_mask(img_seg)[mask_type]
-        if mask_area>0:
-            img_gt_masked=img_gt*img_mask
-            img_input_masked=img_input*img_mask
-            input_mse=mean_squared_error(img_gt_masked, img_input_masked)/(255*255*mask_area)
-            input_psnr=10 * np.log10((1.0 ** 2) / input_mse)
-            metric_dict[mask_type].append(input_psnr)
+def calculate_metrics(args):
+    
+    # logging
+    if not os.path.exists(args['log_path']):
+        os.makedirs(args['log_path'])
+    
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
 
-glare_psnr=mean(metric_dict['glare'])
-streak_psnr=mean(metric_dict['streak'])
-global_psnr=mean(metric_dict['global'])
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
 
-mean_psnr=mean([glare_psnr,streak_psnr,global_psnr])
+    log_filename = os.path.join(args['log_path'], 'evaluate.log')
+    file_handler = logging.FileHandler(log_filename)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    
+    
+    gt_list = os.listdir(args['gt'])
+    input_list = os.listdir(args['input'])
+    if args['mask'] is not None:
+        mask_list = os.listdir(args['mask'])
 
-with open(output_filename, 'w') as f:
-    f.write('{}: {}\n'.format('G-PSNR', glare_psnr))
-    f.write('{}: {}\n'.format('S-PSNR', streak_psnr))
-    f.write('{}:{}\n'.format('global',global_psnr))
-    f.write('{}: {}\n'.format('Score', mean_psnr))
-    f.write('DEVICE: CPU\n')
+    assert len(gt_list) == len(input_list)
+
+    n = len(gt_list)
+
+    score_dict={'glare':0,'streak':0,'global':0,'glare_num':0,'streak_num':0,'global_num':0}
+    for i in range(n):
+        img_gt = io.imread(os.path.join(args['gt'], gt_list[i]))
+        img_input = io.imread(os.path.join(args['input'], input_list[i]))
+
+        if args['mask'] is not None:
+            img_seg=io.imread(os.path.join(args['mask'], mask_list[i]))
+            metric_dict=compare_score(img_gt,img_input,img_seg)
+            for key in metric_dict.keys():
+                score_dict[key]+=metric_dict[key]
+                score_dict[key+'_num']+=1
+
+    if args['mask'] is not None:
+        for key in ['glare','streak','global']:
+            if score_dict[key+'_num'] == 0:
+                assert False, "Error, No mask in this type!"
+            score_dict[key]/= score_dict[key+'_num']
+        score_dict['score']=1/3*(score_dict['glare']+score_dict['global']+score_dict['streak'])
+
+        for key in ['score', 'glare', 'streak', 'global']:
+            logger.info("metrics    " + "\t".join([f"{key}={score_dict[key]:.4f}"]))
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input',type=str,default='./data/test_data/real/input')
+    parser.add_argument('--gt',type=str,default='./data/test_data/real/gt')
+    parser.add_argument('--mask',type=str,default='./data/test_data/real/mask')
+    parser.add_argument('--log_path', type=str, default='./log')
+    args = vars(parser.parse_args())
+    calculate_metrics(args)
